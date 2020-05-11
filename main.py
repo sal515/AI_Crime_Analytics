3395f6d16a8f4ed95cea71b92fa3498495178206
sal515 <salman.rahman515@gmail.com>
2020-05-11 18:49:02 -0400

    Generated a figure of all the data points

--- main.py
+++ main.py
@@ -5,11 +5,33 @@
 # For COMP 472 Section - ABIX – Summer 2020
 # --------------------------------------------------------
 
+# TODO: Move functions to separate package
+# ====== functions ======
+def save_figure(figureName):
+    plt.savefig("".join([figures_dir_path, figureName]))
+
+
 # ====== imports ======
+import geopandas
+import matplotlib.pyplot as plt
+from shapely.geometry import Polygon as polygon
+from shapely.geometry import MultiPoint
+from shapely.geometry import Point
+
+# ====== user input variables ======
+square_grid_length = 0.002
+
 # ====== constant variables ======
+shapes_data_path = "data\\Shape\\crime_dt.shp"
+figures_dir_path = "figures\\"
 # ====== logic ======
 
 # read shape file using geopanda
+crime_data = geopandas.read_file(shapes_data_path)
+
+# Create a figure with all the data points in figure directory of the project
+crime_data.plot()
+save_figure("all_crime_data.png")
 
 
 
