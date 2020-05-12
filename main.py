b27185b4ac7eb668d74731d5d5cf7e815b8c82a1
sal515 <salman.rahman515@gmail.com>
2020-05-12 18:04:06 -0400

    Fixed the upper bound issues for plotting and grid

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 
 # TODO: Move functions to separate package
 # ====== functions ======
+import math
+
+
 def save_figure(plt, figureName):
     plt.savefig("".join([figures_dir_path, figureName]))
 
@@ -39,38 +42,26 @@ x_y_pair = list(map(lambda point: list(point.coords)[0], crime_data.geometry))
 x = list(map(lambda point: point[0], x_y_pair))
 y = list(map(lambda point: point[1], x_y_pair))
 
-# FIXME: Fix the difference in calculation of distance
-max_y_length = Point(min(x), min(y)).distance(Point(min(x), max(y)))
-max_x_length = Point(min(x), min(y)).distance(Point(max(x), min(y)))
-
-# Create a figure with all the data points in figure directory of the project
-# crime_data.geometry.plot(ax=ax)
-fig1 = plt.figure(1)
-ax = fig1.add_subplot(1, 1, 1)
-
-rect_initial = patches.Rectangle((min(x), min(y)), max_x_length, max_y_length, color="purple")
-ax.add_patch(rect_initial)
-plt.axis([(min(x) - grid_buffer), (max(x) + grid_buffer), (min(y) - grid_buffer), (max(y) + grid_buffer)])
-# plt.plot(x, y, "o")
-
-
-# Identifying the bounds of the given data
+# To draw the grid, identifying the bounds of the given data
 # By creating a polygon using all the crime data points
 poly = polygon(x_y_pair)
 bounds = poly.bounds
 print("bounds: (minx-0, miny-1, maxx-2, maxy-3) ", bounds)
 
-# Calculating the x and y points of the grids, to draw the plot for visualization
-# FIXME: CHECK EQUATION - WHEN TO ADD AND WHEN TO SUBTRACT
-col_points = list(np.arange(bounds[0], (bounds[2] + square_grid_length), square_grid_length))
-row_points = list(np.arange(bounds[1], (bounds[3] + square_grid_length), square_grid_length))
+# To draw the grids on the plot, extracting the x and y points of the grids
+cols = int(math.ceil(abs(abs(bounds[2]) - abs(bounds[0])) / square_grid_length))
+rows = int(math.ceil(abs(abs(bounds[3]) - abs(bounds[1])) / square_grid_length))
+
+# FIXME: CHECK EQUATION - WHEN TO ADD AND WHEN TO SUBTRACT - DONE?
+col_points = list(
+    np.arange(bounds[0], (bounds[0] + (cols * square_grid_length) + (square_grid_length / 3)), square_grid_length))
+
+row_points = list(
+    np.arange(bounds[1], (bounds[1] + (rows * square_grid_length) + (square_grid_length / 3)), square_grid_length))
+
 print("column points: ", col_points)
 print("row points: ", row_points)
 
-# FIXME: CHECK EQUATION - Fix this calculation
-# rows = int(abs(abs(bounds[2]) - abs(bounds[0])) / square_grid_length)
-# cols = int(abs(abs(bounds[3]) - abs(bounds[1]+ square_grid_length)) / square_grid_length)
-#
 # print(f"rows: {rows}, cols: {cols}")
 #
 # test_bound = bounds[0]
@@ -81,11 +72,35 @@ print("row points: ", row_points)
 #     test_bound += square_grid_length
 #     print(test_bound)
 
+# To draw initial purple patch size,calculate the rectangular grid length & width
+# max_y_length = Point(0, bounds[1]).distance(Point(0, bounds[3]))
+# max_x_length = Point(bounds[0], 0).distance(Point(bounds[2], 0))
+# TODO: The given cordinate is not a perfect square
+# max_y_length = abs(abs(bounds[3]) - abs(bounds[1]))
+# max_x_length = abs(abs(bounds[2]) - abs(bounds[0]))
+
+max_y_length = abs(abs((bounds[1] + (rows * square_grid_length))) - abs(bounds[1]))
+max_x_length = abs(abs((bounds[0] + (cols * square_grid_length))) - abs(bounds[0]))
+
+# For visualization:Create a figure with all the data stored in "figure directory"
+# crime_data.geometry.plot(ax=ax)
+fig1 = plt.figure(1)
+ax = fig1.add_subplot(1, 1, 1)
+
+rect_initial = patches.Rectangle((min(x), min(y)), max_x_length, max_y_length, color="purple")
+ax.add_patch(rect_initial)
+
+# Figure axis bounds
+plt.axis([(min(x) - grid_buffer), (max(x) + grid_buffer), (min(y) - grid_buffer), (max(y) + grid_buffer)])
+
+# Plot points
+plt.plot(x, y, "o")
+
 # Drawing grids
-# for i in col_points:
-#     plt.axvline(x=i)
-# for i in row_points:
-#     plt.axhline(y=i)
+for i in col_points:
+    plt.axvline(x=i)
+for i in row_points:
+    plt.axhline(y=i)
 
 # TODO: figure out why lambda doesn't work
 # map(lambda col: plt.axvline(x=col), col_points)
@@ -104,7 +119,7 @@ rect_high_risk = patches.Rectangle((min(x), min(y)), square_grid_length, square_
 ax.add_patch(rect_high_risk)
 
 # Display plot
-# save_figure(plt, "all_crime_data.png")
+save_figure(plt, "all_crime_data.png")
 plt.show()
 
 # ===== end of program =====
