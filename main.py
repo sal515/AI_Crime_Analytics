c0f2cfc604315105c9b162cb471954e492630717
sal515 <salman.rahman515@gmail.com>
2020-05-12 02:53:18 -0400

    Generated grids and colored patches for Visualization

--- main.py
+++ main.py
@@ -7,43 +7,105 @@
 
 # TODO: Move functions to separate package
 # ====== functions ======
-def save_figure(figureName):
+def save_figure(plt, figureName):
     plt.savefig("".join([figures_dir_path, figureName]))
 
 
 # ====== imports ======
 import geopandas
+import matplotlib.axes as axes
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 from shapely.geometry import Polygon as polygon
 from shapely.geometry import MultiPoint
 from shapely.geometry import Point
+import numpy as np
 
 # ====== user input variables ======
 square_grid_length = 0.002
+grid_buffer = square_grid_length * 2
 
-# ====== constant variables ======
+# ====== constant path variables ======
 shapes_data_path = "data\\Shape\\crime_dt.shp"
 figures_dir_path = "figures\\"
 
-# Read shape file using geopanda as a dataframe
-crime_data = geopandas.read_file(shapes_data_path)
-# Extract all the points from the data frame as a list of points
-all_points = list(map(lambda point: list(point.coords)[0], crime_data.geometry))
-
 # ====== logic ======
 
+# Read shape file using geopanda as a dataframe
+crime_data = geopandas.read_file(shapes_data_path)
+
+# Extract all the points from the data frame as a list of points
+x_y_pair = list(map(lambda point: list(point.coords)[0], crime_data.geometry))
+x = list(map(lambda point: point[0], x_y_pair))
+y = list(map(lambda point: point[1], x_y_pair))
+
+# FIXME: Fix the difference in calculation of distance
+max_y_length = Point(min(x), min(y)).distance(Point(min(x), max(y)))
+max_x_length = Point(min(x), min(y)).distance(Point(max(x), min(y)))
 
 # Create a figure with all the data points in figure directory of the project
-crime_data.plot()
-save_figure("all_crime_data.png")
+# crime_data.geometry.plot(ax=ax)
+fig1 = plt.figure(1)
+ax = fig1.add_subplot(1, 1, 1)
+
+rect_initial = patches.Rectangle((min(x), min(y)), max_x_length, max_y_length, color="purple")
+ax.add_patch(rect_initial)
+plt.axis([(min(x) - grid_buffer), (max(x) + grid_buffer), (min(y) - grid_buffer), (max(y) + grid_buffer)])
+# plt.plot(x, y, "o")
+
 
 # Identifying the bounds of the given data
 # By creating a polygon using all the crime data points
-# all_points = list(map(lambda point: list(point.coords)[0], crime_data.geometry))
-poly = polygon(all_points)
-print(poly.bounds)
+poly = polygon(x_y_pair)
+bounds = poly.bounds
+print("bounds: (minx-0, miny-1, maxx-2, maxy-3) ", bounds)
+
+# Calculating the x and y points of the grids, to draw the plot for visualization
+# FIXME: CHECK EQUATION - WHEN TO ADD AND WHEN TO SUBTRACT
+col_points = list(np.arange(bounds[0], (bounds[2] + square_grid_length), square_grid_length))
+row_points = list(np.arange(bounds[1], (bounds[3] + square_grid_length), square_grid_length))
+print("column points: ", col_points)
+print("row points: ", row_points)
+
+# FIXME: CHECK EQUATION - Fix this calculation
+# rows = int(abs(abs(bounds[2]) - abs(bounds[0])) / square_grid_length)
+# cols = int(abs(abs(bounds[3]) - abs(bounds[1]+ square_grid_length)) / square_grid_length)
+#
+# print(f"rows: {rows}, cols: {cols}")
+#
+# test_bound = bounds[0]
+# for i in range(0, rows):
+#     if i == 0:
+#         print(test_bound)
+#         continue
+#     test_bound += square_grid_length
+#     print(test_bound)
+
+# Drawing grids
+# for i in col_points:
+#     plt.axvline(x=i)
+# for i in row_points:
+#     plt.axhline(y=i)
+
+# TODO: figure out why lambda doesn't work
+# map(lambda col: plt.axvline(x=col), col_points)
+# FIXME: Fix the grid lines to span only on the data points area
+# start_point = [col_points[3], row_points[0]]
+# end_point = [col_points[3], row_points[(row_points.__len__() - 1)]]
+# plt.plot(start_point, end_point)
 
 
+# Add rectanges for visualization
+
+rect_high_risk = patches.Rectangle((min(x), min(y)), square_grid_length, square_grid_length, color="red")
+ax.add_patch(rect_high_risk)
+
+rect_high_risk = patches.Rectangle((min(x), min(y)), square_grid_length, square_grid_length, color="yellow")
+ax.add_patch(rect_high_risk)
+
+# Display plot
+# save_figure(plt, "all_crime_data.png")
+plt.show()
 
 # ===== end of program =====
-print("program terminated")
+print("=== program terminated ===")
