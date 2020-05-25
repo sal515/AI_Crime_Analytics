f4eb3b41cd8a9e236c1eed76dffdce8644cd25cb
sal515 <salman.rahman515@gmail.com>
2020-05-25 16:54:09 -0400

    Fixed issue with map having multiple keys and Updated closed list at the beginning to have all the forbidden nodes

--- path_finding/a_star_algo.py
+++ path_finding/a_star_algo.py
@@ -1,55 +1,111 @@
+import itertools
+
 from path_finding.node import node
 from path_finding.priority_queue_helper import pq_helper
 import queue as q
+from collections import defaultdict
 
 
 class aStar:
 
-    def __init__(self, start, destination, obstacles_array) -> None:
+    def __init__(self, start, destination, obstacles_array, data) -> None:
         # eg.
         # start -> node(None,None,  (0,0))
         # destination -> node(None, None, (9,3))
 
-        self.open_priority_queue = q.PriorityQueue(-1)
-        self.open_map = {}
-        # self.open_priority_queue = []
-        self.closed_list = []
-        self.closed_map = {}
+        self.path = []
 
-        self.obst_arr = obstacles_array
+        self.open_priority_queue = q.PriorityQueue(-1)
+        # self.open_dict = {}
+        # FIXME: Check anything related to the open dict and duplicate keys
+        self.open_dict = defaultdict(list)
+        self.closed_list = []
+        self.closed_dict = {}
+
+        self.data = data
+        self.rows = data.rows
+        self.cols = data.cols
+        # assuming the boundary is of a square or rectangle
+        self.forbidden_nodes = (self.rows * 2 + self.cols * 2) - 4
+
+        self.obstacle_array = obstacles_array
         self.start: node = node(parent=None, destination=destination, position=start)
         self.destination: node = node(parent=None, destination=destination, position=destination)
 
         self.node_possibilities = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, 1), (1, 1), (-1, -1), (1, -1)]
 
     def run(self):
-        pq_helper.add_node(self.start, self.open_priority_queue, self.open_map)
+        # insert blocked/corner nodes to the closed list
+        self.update_forbidden_nodes()
+
+        pq_helper.add_node(self.start, self.open_priority_queue, self.open_dict)
 
         while not self.open_priority_queue.empty():
             # get current node from openList with lowest f
-            # remove the current node from the open list & map
-            current_node: node = pq_helper.pop_node(self.open_priority_queue, self.open_map)
-            # add the current node to the closed list & map
-            self.closed_map[str(hash(current_node))] = current_node
-            self.closed_list.append(current_node)
+            # remove the current node from the open list & dict
+            current_node: node = pq_helper.pop_node(self.open_priority_queue, self.open_dict)
+            # add the current node to the closed list & dict
+            self.update_closed_list(current_node)
 
             if current_node == self.destination:
-                # FIXME: goal state found - use closed list to find shortest path to goal
-                break
+                backtrace_node = current_node
+                while not backtrace_node == self.start:
+                    self.path.append(backtrace_node)
+                    backtrace_node = backtrace_node.parent
+                # return shortest path
+                return self.path[::-1]
 
             for possible_node in self.node_possibilities:
                 adjacent_node = self.get_adjacent_node(current_node, possible_node)
+                adjacent_node_key = str(hash(adjacent_node))
 
-                if str(hash(adjacent_node)) in self.closed_map:
+                if adjacent_node_key in self.closed_dict:
                     continue
 
-                if (str(hash(adjacent_node)) in self.open_map) and (
-                        adjacent_node.g > self.open_map[str(hash(adjacent_node))].g):
-                    continue
+                # Fixme : duplicate keys for the map
+                if adjacent_node_key in self.open_dict:
+                    for node_entry in self.open_dict[adjacent_node_key]:
+                        if adjacent_node.g > node_entry[2].g:
+                            continue
 
-                # FIXME duplicate - How?
-                # self.open_map[str(hash(adjacent_node))] = adjacent_node
-                pq_helper.add_node(adjacent_node, self.open_priority_queue, self.open_map)
+                    # nodes_list = self.open_dict[adjacent_node_key]
+                    # if len(nodes_list) < 2:
+                    #     if adjacent_node.g > nodes_list[0][2].g:
+                    #         continue
+
+                    # index = itertools.count()
+                    # # nodes_list = self.open_dict[adjacent_node_key]
+                    # for node_entry in self.open_dict[adjacent_node_key]:
+                    #     i = next(index)
+                    #     if node_entry[0] == ad:
+
+                    # ((adjacent_node.g > self.open_dict[adjacent_node_key].g) or (
+                    # adjacent_node.g == self.open_dict[adjacent_node_key].g)):
+
+                pq_helper.add_node(adjacent_node, self.open_priority_queue, self.open_dict)
+
+    # === Helper functions ===
+
+    def update_forbidden_nodes(self):
+        for r in (0, self.rows - 1):
+            for c in range(0, self.cols - 1):
+                # self.obstacle_array[self.data.to_index(r,c,self.cols)]
+                self.update_closed_list(node(None, None, (r, c)))
+        for c in (0, self.cols - 1):
+            for r in range(0, self.rows - 1):
+                # self.obstacle_array[self.data.to_index(r,c,self.cols)]
+                self.update_closed_list(node(None, None, (r, c)))
+        blocked_grids_r_c = [self.data.to_row_col(i, self.data.cols) for i in range(len(self.obstacle_array)) if
+                             self.obstacle_array[i] == 0]
+        for r_c in blocked_grids_r_c:
+            self.update_closed_list(node(None, None, r_c))
+
+    def update_closed_list(self, current_node):
+        current_node_key = str(hash(current_node))
+        if current_node_key in self.closed_dict:
+            return
+        self.closed_dict[current_node_key] = current_node
+        self.closed_list.append(current_node)
 
     def get_adjacent_node(self, current_node: node, possible_node):
         return node(current_node, self.destination,
